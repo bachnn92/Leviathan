5bc50c90da86668a369596ba30112a93f0a65c19
bachnn92 <bachnn92@gmail.com>
2024-11-20 21:55:50 +0700

    import file

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    extra.print_input_message("This is a message from the extra file.")
 
---
+++ extra.py
@@ -0,0 +1,6 @@
+# /home/bachnn92/source/Leviathan/extra.py
+def print_message():
+    print("This is a message from the extra file.")
+
+def print_input_message(input_message):
+    print(f"Input message: {input_message}")
